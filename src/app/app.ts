6328dd0f7bfe201a61fd7dd2574d2df3e58eb8ce
bingo_zhou <bingoabs@gmail.com>
2019-01-03 14:17:56 +0800

    Update the static files path

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,9 @@ export class AppFactory {
     }
     createApp(){
         this.ncp.limit = 16;
-        let result = this.ncp("./tornado-starter/src/templates", this.appPath, function(err: any) {
+        //  TODO: make the static file packaged into the release
+        let template_path = "/Users/bingokarl/project/tornado-starter/src/templates"
+        let result = this.ncp(template_path, this.appPath, function(err: any) {
             if (err){
                 console.error(err);
             }
